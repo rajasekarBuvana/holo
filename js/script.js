da1f861bbf53afe7daa2ed7e6f0b3180497ec66b
rajasekarBuvana <rajasekarbuvana161@gmail.com>
2021-07-19 23:51:19 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+/* Open */
+function openNav(){
+    document.getElementById("myNav").style.height = "100%";
+}
+
+// close 
+function closeNav(){
+    document.getElementById("myNav").style.height = "0%";
+}
+ 
